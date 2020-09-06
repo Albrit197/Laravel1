3d7fed3c34bd8853ca3a1dd7f874d8723c67eba6
Albrit197 <63236021+Albrit197@users.noreply.github.com>
2020-09-06 10:15:26 +0300

    initial comit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
+{{-- <h1>{{ $var }}</h1>  --}}
+    	<h1>Hello World!</h1>
+<h1>{{ 2 == 3 ? "Equals" : "Does not equal" }}</h1>
+
+@endsection 
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+$route_prefix = '/test/'; // reikalingas todėl, kad kai ateiname iš localost/app, tai url nėra “/”, bet “/app
+Route::get($route_prefix . '/', function () {
+    return view('welcome');
+});
